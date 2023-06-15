edf229e7ca83c20611685313306a5da96e451150
burakhanunver <burakhanunver@gmail.com>
2023-06-15 15:47:29 +0300

    add responsiveness to navbar

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
 export default function Home() {
-  return <main className="">Welcome to the homepage of blog</main>
+  return <main className="container mx-auto">
+    Welcome to the homepage of blog
+    </main>
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,11 +2,11 @@ import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-center space-x-4 items-center bg-blue-900 h-12">
+    <nav className="flex px-2 justify-between lg:py-0 lg:justify-center space-x-4 items-center bg-blue-900 h-12">
       <Link href="/" className="text-xl font-bold mx-2 text-white">
         Next Blog
       </Link>
-      <div className="flex text-lg text-center space-x-6 text-white">
+      <div className="hidden lg:flex text-lg text-center space-x-6 text-white">
         <Link href="/contact">Contact</Link>
         <Link href="/about">About</Link>
         <a href="https://github.com/brkunver/next-static-blog">Github Repo</a>
@@ -14,6 +14,7 @@ const Navbar = () => {
         <a href="https://nextjs.org/docs">Next.js Docs</a>
         <a href="https://jsonplaceholder.typicode.com/">Json Placeholder</a>
       </div>
+      <button className="lg:hidden text-white">More...</button>
     </nav>
   )
 }
