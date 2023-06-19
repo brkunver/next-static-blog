d4d43e66fe4f3dae1b6247fcbc4fb6864048f914
burakhanunver <burakhanunver@gmail.com>
2023-06-19 11:05:15 +0300

    change font

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,16 @@
 import "./globals.css"
 import Navbar from "@/components/Navbar"
+
 export const metadata = {
   title: "Next Static Blog",
   description: "Nextjs Static Blog app",
 }
 
+
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" >
       <body>
         <Navbar />
         {children}
